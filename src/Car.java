b4a31bf9a77b1c691ee5deee090cf6d905a92bb4
iladanilenko <Danilenkoi@inbox.ru>
2023-01-13 15:55:15 +0300

    Задание 3.

--- src/Car.java
+++ src/Car.java
@@ -1,19 +1,49 @@
 public class Car {
 
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int productionYear;
-    String productionCountry;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int productionYear;
+    private String productionCountry;
 
     public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
+
+        if (brand == null || brand.isBlank()){
+            this.brand = "Brand";
+        }else {
+            this.brand = brand;
+        }
+
+        if (model == null || model.isBlank()){
+            this.model = "Model";
+        }else {
+            this.model = model;
+        }
+
+        if (engineVolume <= 0){
+            this.engineVolume = 1.5;
+        }else {
+            this.engineVolume = engineVolume;
+        }
+
+        if (color == null || color.isBlank()){
+            this.color = "white";
+        }else{
+            this.color = color;
+        }
+
+        if (productionYear <= 0){
+            this.productionYear = 2000;
+        }else {
+            this.productionYear = productionYear;
+        }
+
+        if (productionCountry == null || productionCountry.isBlank()){
+            this.productionCountry = "Made in ***";
+        }else {
+            this.productionCountry = productionCountry;
+        }
     }
 
     public String toString () {
